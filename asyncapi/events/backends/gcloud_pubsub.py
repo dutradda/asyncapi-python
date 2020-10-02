9c65796088a618736c77b9e0ff0c55d80a23b496
Diogo Dutra <diogo.albuquerque@luizalabs.com>
2020-10-02 05:43:49 -0300

    Improve gcloud pubsub backend

--- asyncapi/events/backends/gcloud_pubsub.py
+++ asyncapi/events/backends/gcloud_pubsub.py
@@ -1,24 +1,28 @@
 import asyncio
 import functools
-from concurrent.futures import Future, ThreadPoolExecutor
+import itertools
+from collections import defaultdict
 from concurrent.futures import TimeoutError as FutureTimeoutError
 from logging import Logger, getLogger
-from typing import TYPE_CHECKING, Any, Dict, Optional
+from typing import TYPE_CHECKING, Any, DefaultDict, Dict, Optional, Set, Tuple
 from urllib.parse import urlparse
 
 from broadcaster._backends.base import BroadcastBackend
 from google.cloud import pubsub_v1
 from google.cloud.pubsub_v1.types import PullResponse, ReceivedMessage
 
-from asyncapi import PublishTimeoutError
+from asyncapi import (
+    GCloudPubSubConsumerDisconnectError,
+    GCloudPubSubPublishTimeoutError,
+)
 
 from .. import Event
 
 
 if TYPE_CHECKING:
-    FutureHint = Future[None]
+    AsyncFutureHint = asyncio.Future[PullResponse]
 else:
-    FutureHint = Future
+    AsyncFutureHint = asyncio.Future
 
 
 class GCloudPubSubBackend(BroadcastBackend):
@@ -27,29 +31,34 @@ class GCloudPubSubBackend(BroadcastBackend):
         url: str,
         bindings: Dict[str, str] = {},
         logger: Logger = getLogger(__name__),
-        executor: Optional[ThreadPoolExecutor] = None,
     ):
         url_parsed = urlparse(url, scheme='gcloud-pubsub')
         self._project = url_parsed.netloc
         self._consumer_channels: Dict[str, str] = {}
         self._producer_channels: Dict[str, str] = {}
         self._channel_index = 0
-        self._should_stop = True
         self._logger = logger
         self._set_consumer_config(bindings)
-
-        if executor is None:
-            executor = ThreadPoolExecutor(self._consumer_max_workers)
-
-        self._executor = executor
+        self._pull_message_futures: DefaultDict[
+            str, Set[AsyncFutureHint]
+        ] = defaultdict(set)
+        self._disconnected = True
 
     async def connect(self) -> None:
         self._producer = pubsub_v1.PublisherClient()
         self._consumer = pubsub_v1.SubscriberClient()
-        self._should_stop = False
+        self._disconnected = False
 
     async def disconnect(self) -> None:
-        self._should_stop = True
+        self._disconnected = True
+
+        for future in itertools.chain(*self._pull_message_futures.values()):
+            try:
+                await future
+            except asyncio.exceptions.CancelledError:
+                ...
+
+        self._pull_message_futures.clear()
         self._producer.stop()
         self._consumer.close()
         del self._producer
@@ -62,6 +71,9 @@ class GCloudPubSubBackend(BroadcastBackend):
         self._consumer_channels[channel] = pubsub_channel
 
     async def unsubscribe(self, channel: str) -> None:
+        for future in self._pull_message_futures[channel]:
+            future.cancel()
+
         self._consumer_channels.pop(channel)
 
     async def publish(
@@ -81,7 +93,7 @@ class GCloudPubSubBackend(BroadcastBackend):
             future.result(timeout=self._publish_timeout)
         except FutureTimeoutError:
             if retries_counter >= self._publish_retries:
-                raise PublishTimeoutError(
+                raise GCloudPubSubPublishTimeoutError(
                     f'publish timeout; channel={channel}; '
                     f'message={message[:100]}...'
                 )
@@ -89,72 +101,67 @@ class GCloudPubSubBackend(BroadcastBackend):
                 await self.publish(channel, message, retries_counter + 1)
 
     async def next_published(self) -> Optional[Event]:
-        channel_index = self._channel_index
+        (
+            response,
+            channel_id,
+            pubsub_channel,
+        ) = await self._pull_message_from_consumer()
+        received_message = response.received_messages[0]
+        event = Event(channel_id, received_message.message.data.decode())
 
-        while not self._should_stop:
-            channels = list(self._consumer_channels.items())
+        if self._consumer_ack_messages:
+            await self.wait_ack(received_message, pubsub_channel)
+        else:
+            event.context['ack_func'] = functools.partial(
+                self.wait_ack, received_message, pubsub_channel,
+            )
 
-            if not channels:
+        return event
+
+    async def _pull_message_from_consumer(
+        self,
+    ) -> Tuple[PullResponse, str, str]:
+        channels = list(self._consumer_channels.items())
+        channel_index = 0
+
+        while not self._disconnected:
+            if channel_index >= len(channels):
+                channel_index = 0
+
+            try:
+                channel_id, pubsub_channel = channels[channel_index]
+            except IndexError:
                 await asyncio.sleep(self._consumer_wait_time)
                 continue
 
-            channel_id, pubsub_channel = channels[channel_index]
-
-            response = await self._pull_message_from_consumer(pubsub_channel)
-
-            if response is None or not response.received_messages:
-                await asyncio.sleep(self._consumer_wait_time)
-                if self._channel_index >= len(channels) - 1:
-                    channel_index = self._channel_index = 0
-                else:
-                    self._channel_index += 1
-                    channel_index = self._channel_index
-
-            else:
-                received_message = response.received_messages[0]
-                event = Event(
-                    channel_id, received_message.message.data.decode()
-                )
-
-                if self._consumer_ack_messages:
-                    await self.wait_ack(received_message, pubsub_channel)
-                else:
-                    event.context['ack_func'] = functools.partial(
-                        self.wait_ack, received_message, pubsub_channel,
-                    )
-
-                return event
-
-        return None
-
-    async def _pull_message_from_consumer(
-        self, pubsub_channel: str, retries_counter: int = 1,
-    ) -> Optional[PullResponse]:
-        future = asyncio.get_running_loop().run_in_executor(
-            None,
-            functools.partial(
-                self._consumer.pull,
-                pubsub_channel,
-                max_messages=1,
-                return_immediately=True,
-            ),
-        )
-
-        try:
-            return await asyncio.wait_for(
-                future, timeout=self._consumer_pull_message_timeout
+            pull_message_future: AsyncFutureHint
+            pull_message_future = asyncio.get_running_loop().run_in_executor(  # type: ignore
+                None,
+                functools.partial(
+                    self._consumer.pull, pubsub_channel, max_messages=1,
+                ),
             )
-        except asyncio.TimeoutError:
-            if retries_counter >= self._consumer_pull_message_retries:
-                self._logger.warning(
-                    f'pull message timeout {self._consumer_pull_message_timeout}; '
-                    f'channel={pubsub_channel}'
+
+            self._pull_message_futures[channel_id].add(pull_message_future)
+
+            try:
+                response = await asyncio.wait_for(
+                    pull_message_future, self._consumer_pull_message_timeout
                 )
-                return None
+                self._pull_message_futures[channel_id].remove(
+                    pull_message_future
+                )
+            except asyncio.TimeoutError:
+                self._pull_message_futures[channel_id].remove(
+                    pull_message_future
+                )
+                channel_index += 1
+                await asyncio.sleep(self._consumer_wait_time)
+                continue
             else:
-                return await self._pull_message_from_consumer(
-                    pubsub_channel, retries_counter + 1
-                )
+                return response, channel_id, pubsub_channel
+
+        raise GCloudPubSubConsumerDisconnectError()
 
     async def wait_ack(
         self,
@@ -181,15 +188,18 @@ class GCloudPubSubBackend(BroadcastBackend):
                 await self.wait_ack(
                     message, pubsub_channel, retries_counter + 1
                 )
+        except asyncio.CancelledError:
+            self._logger.warning(
+                'ack cancelled; '
+                f'message={message.message.data.decode()[:100]}...'
+            )
 
     def _set_consumer_config(self, bindings: Dict[str, str]) -> None:
         consumer_wait_time = 1.0
         consumer_ack_messages = False
         consumer_ack_timeout = 5.0
         consumer_ack_retries = 3
-        consumer_max_workers = 10
-        consumer_pull_message_timeout = 1.0
-        consumer_pull_message_retries = 3
+        consumer_pull_message_timeout = 0.5
         publish_timeout = 5.0
         publish_retries = 3
 
@@ -213,14 +223,8 @@ class GCloudPubSubBackend(BroadcastBackend):
             elif config_name == 'consumer_ack_retries':
                 consumer_ack_retries = int(config_value)
 
-            elif config_name == 'consumer_max_workers':
-                consumer_max_workers = int(config_value)
-
             elif config_name == 'consumer_pull_message_timeout':
                 consumer_pull_message_timeout = float(config_value)
-
-            elif config_name == 'consumer_pull_message_retries':
-                consumer_pull_message_retries = int(config_value)
 
             elif config_name == 'publish_timeout':
                 publish_timeout = float(config_value)
@@ -232,8 +236,6 @@ class GCloudPubSubBackend(BroadcastBackend):
         self._consumer_ack_messages = consumer_ack_messages
         self._consumer_ack_timeout = consumer_ack_timeout
         self._consumer_ack_retries = consumer_ack_retries
-        self._consumer_max_workers = consumer_max_workers
         self._consumer_pull_message_timeout = consumer_pull_message_timeout
-        self._consumer_pull_message_retries = consumer_pull_message_retries
         self._publish_timeout = publish_timeout
         self._publish_retries = publish_retries
